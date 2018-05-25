80bad3b1c98a7085b2e036261af59277cbd29a29
Andre Britton <andre.britton@wessexwater.co.uk>
2018-05-25 14:14:15 +0100

    Added d3 liquid gauges

--- resources/js/logo.js
+++ resources/js/logo.js
@@ -1,6 +1,10 @@
-var DomBody = document.getElementById('loading-div');
-var two = new Two({fullscreen:true}).appendTo(DomBody);
-var Rect1 = two.makeRectangle(100,100,100,100);
-//Rect1.fill('black');
-//Rect1.noStroke();
-two.update();
+function ShowLogo(){
+  var config1 = liquidFillGaugeDefaultSettings();
+  config1.textSize = 0;
+  config1.displayPercent = false;
+  config1.waveRise = false;
+  config1.waveHeight = 0.1;
+  config1.waveAnimateTime = 6000;
+  var gauge1 = loadLiquidFillGauge("loading-gauge", 60, config1);
+};
+ShowLogo();
